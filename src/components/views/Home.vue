11da5f335b68a32162020fa2816a22792ab8a574
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-12-05 10:49:14 +0100

    Added ability to search for a particular user

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,23 +1,64 @@
 <template>
   <div>
-    <h1>Première page VueJS</h1>
-  	<pre>{{retoursAPI}}</pre>
+    <h1>Dépôts utilisateurs</h1>
+    <!-- <pre>{{retoursAPI}}</pre> -->
+  <pre v-if="error !== null">Erreur: {{error}}</pre>
+    <div>
+      <label for="name">Nom : </label>
+      <input type="text" 
+             id="name"
+             name="name"
+             v-model="nameToSearch"
+             @keypress.enter="search"
+             :disabled="loading"/>
+      <button @click="search" :disabled="loading">
+        Rechercher
+      </button>
+
+    </div>
+
+    <hr>
+
+  <div v-if="!loading">
+    <div v-for="repo in retoursAPI">
+      <div>Id: {{repo.id}}</div>
+      <div>Name: {{repo.name}}</div>
+      <hr>
+    </div>
+  </div>
+  <div v-else>Chargement...</div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  data (){
-  	return {
-      retoursAPI: {}
-  	}
+  data () {
+    return {
+      retoursAPI: {},
+      nameToSearch: '',
+      loading: false,
+      error: null
+    }
   },
-  created (){
-  	this.$http.get('https://api.github.com/users/mtancoigne/repos')
-  	.then((data) => {
-  		this.retoursAPI = data.body
-  	})
+  methods: {
+    search () {
+      this.loading = true
+      this.retoursAPI = {}
+      // Définition de l'URL avec le nom donné
+      const URL = `https://api.github.com/users/${this.nameToSearch}/repos`
+
+      // Récupération des données de l'API
+      this.$http.get(URL)
+      .then((data) => {
+        this.retoursAPI = data.body
+        this.loading = false
+      })
+      .catch((error) => {
+        this.error = error
+      })
+    }
   }
 }
 </script>
